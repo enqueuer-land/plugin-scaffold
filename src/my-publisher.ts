f3fa6c824e80b5a9c10c69748c0720b02f54ef62
Guilherme Virgs Moraes <guilherme.moraes@outlook.com>
2020-03-17 12:13:21 -0300

    Update my-publisher.ts

--- src/my-publisher.ts
+++ src/my-publisher.ts
@@ -6,10 +6,15 @@ export class MyPublisher extends Publisher {
         super(publisherProperties);
     }
 
-    public async publish(): Promise<void> {
+    public async publish(): Promise<any> {
         //Publish it to somewhere
         this.executeHookEvent('onFirstHook', {revertedFirst: this.first.split('').reverse().join('')});
         this.executeHookEvent('onSecondHook', {second: this.second.split('').join('.')});
+        
+        //This value below is printed in 'onFinish::publisher' test as 'prove' it actually sent something.
+        // If no value is returned but the promise does not fail, a default value is printed.
+        return {value: 'it has published'};
+
     }
 
 }
